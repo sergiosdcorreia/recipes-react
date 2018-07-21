13acc468eb276e71fda8bd60d3eff4454630b4ad
sergiosdcorreia <sergiosdcorreia@gmail.com>
2018-07-21 21:13:53 +0100

    Add file loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Sergio from './static/images/sergio.jpg';
 
 class App extends React.Component {
     render() {
         return (
-            <div>Recipes App</div>
+            <div>
+                <h1>Recipes App</h1>
+                <img src={Sergio} />
+            </div>
         );
     }
 }
