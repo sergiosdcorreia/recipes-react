3484f7fadb3949cada3ddd2003e441d8e63a6d97
sergiosdcorreia <sergiosdcorreia@gmail.com>
2018-07-24 10:01:49 +0100

    Add prettier

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import propTypes from 'prop-types';
 import classNames from 'classnames';
 
-const RecipeDetail = (props) => {
+const RecipeDetail = props => {
   if (!props.recipe) {
     return (
       <p
@@ -19,44 +19,22 @@ const RecipeDetail = (props) => {
       style={props.style}
       className={classNames('p2 bg-white', props.className)}
     >
-      <h2 className="h2">
-        {props.recipe.name}
-      </h2>
-      <img
-        className="fit"
-        src={props.recipe.image}
-        alt={props.recipe.name}
-      />
+      <h2 className="h2">{props.recipe.name}</h2>
+      <img className="fit" src={props.recipe.image} alt={props.recipe.name} />
       <div>
-        <span>
-          {props.recipe.category}
-        </span>
-        <span>
-          {props.recipe.calories}
-        </span>
+        <span>{props.recipe.category}</span>
+        <span>{props.recipe.calories}</span>
       </div>
 
-      <h3>
-        Ingredients
-      </h3>
+      <h3>Ingredients</h3>
       <ul>
         {props.recipe.ingredients.map(ingredient => (
-          <li key={ingredient}>
-            {ingredient}
-          </li>
+          <li key={ingredient}>{ingredient}</li>
         ))}
       </ul>
 
-      <h3>
-        Steps
-      </h3>
-      <ol>
-        {props.recipe.steps.map(step => (
-          <li key={step}>
-            {step}
-          </li>
-        ))}
-      </ol>
+      <h3>Steps</h3>
+      <ol>{props.recipe.steps.map(step => <li key={step}>{step}</li>)}</ol>
     </div>
   );
 };
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,35 +1,27 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const RecipeList = (props) => {
-  return (
-    <div style={props.style}>
-      <h2 className="h2">
-        Recipes
-      </h2>
-      <ul className="list-reset">
-        {props.recipes.map(recipe => (
-          <li
-            key={recipe.id}
-            className="py2 border-bottom border-bottom-dashed pointer"
-            onClick={() => props.onClick(recipe.id)}
-          >
-            <span>
-              {recipe.name}
-            </span>
-            <span>
-              {recipe.category}
-            </span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+const RecipeList = props => (
+  <div style={props.style}>
+    <h2 className="h2">Recipes</h2>
+    <ul className="list-reset">
+      {props.recipes.map(recipe => (
+        <li
+          key={recipe.id}
+          className="py2 border-bottom border-bottom-dashed pointer"
+          onClick={() => props.onClick(recipe.id)}
+        >
+          <span>{recipe.name}</span>
+          <span>{recipe.category}</span>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 RecipeList.propTypes = {
   style: propTypes.object,
-  recipe: propTypes.object,
+  recipes: propTypes.array,
 };
 
 export default RecipeList;
